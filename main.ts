311c65779dda3d7645e42f0683e5d743c500bd72
Jay-Tech-tech <arne.jay.leonidas@gmail.com>
2026-04-29 10:01:25 +0200

    pause eingebaut

--- main.ts
+++ main.ts
@@ -1,19 +1,28 @@
-let Aktiv = 0
 let Verfolgung = 0
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    Aktiv = 1
-})
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+let Aktiv = 0
+let Entfernung = 0
+input.onButtonEvent(Button.B, input.buttonEventValue(ButtonEvent.Up), function () {
     Verfolgung = 1
 })
+input.onButtonEvent(Button.A, input.buttonEventValue(ButtonEvent.Up), function () {
+    Aktiv = 1
+})
 basic.forever(function () {
+    Entfernung = maqueen.ultrasonic(maqueen.DistanceUnit.Centimeters)
+    if (input.pinIsPressed(TouchPin.P1) && input.pinIsPressed(TouchPin.P1)) {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.writeLED(maqueen.Led.LedAll, maqueen.LedSwitch.LedOff)
+    }
     if (Aktiv == 1) {
-        if (maqueen.ultrasonic(maqueen.DistanceUnit.Centimeters) < 3) {
+        if (Entfernung < 3) {
             basic.showIcon(IconNames.ArrowSouth)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
             maqueen.writeLED(maqueen.Led.LedAll, maqueen.LedSwitch.LedOn)
             basic.pause(1000)
         }
+        basic.showIcon(IconNames.ArrowNorth)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+        basic.pause(1000)
     }
     if (Verfolgung == 1) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft, maqueen.Brightness.Bright) && maqueen.readPatrol(maqueen.Patrol.PatrolRight, maqueen.Brightness.Dark)) {
