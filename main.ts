32dfb6603936c75d347f3bc72ca856b6ba15e456
Jay-Tech-tech <arne.jay.leonidas@gmail.com>
2026-04-29 10:15:20 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,14 +15,15 @@ basic.forever(function () {
     }
     if (Aktiv == 1) {
         if (Entfernung > 3) {
+            basic.showIcon(IconNames.ArrowNorth)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+            basic.pause(500)
+        } else if (Entfernung < 3) {
             basic.showIcon(IconNames.ArrowSouth)
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
             maqueen.writeLED(maqueen.Led.LedAll, maqueen.LedSwitch.LedOn)
             basic.pause(500)
         }
-        basic.showIcon(IconNames.ArrowNorth)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
-        basic.pause(500)
     }
     if (Verfolgung == 1) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft, maqueen.Brightness.Bright) && maqueen.readPatrol(maqueen.Patrol.PatrolRight, maqueen.Brightness.Dark)) {
